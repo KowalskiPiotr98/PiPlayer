fa8e9df3533e12715e61b1bf580359acad9469b0
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-05-03 01:10:33 +0200

    Wrap name and volume with getters and setters

--- PiPlayer/PiPlayer/player.py
+++ PiPlayer/PiPlayer/player.py
@@ -5,16 +5,16 @@ mpc_mutex = threading.Lock()
 
 class Player(object):
     def __init__(self, host = 'localhost', port = 6600):
-        self.name = None
         self._host = host
         self._port = port
         self.client = mpd.MPDClient()
         with mpc_mutex:
+            self._name = None
             self.client.connect(host, port)
             self.client.stop()
-            self.volume = 100
+            self._volume = 100
             self.playing = False
-            self.client.setvol(self.volume)
+            self.client.setvol(self._volume)
             self.client.disconnect()
 
     def change_radio(self, url = None, name = None):
@@ -22,7 +22,7 @@ class Player(object):
             return
         with mpc_mutex:
             self.playing = True
-            self.name = name
+            self._name = name
             self.client.connect(self._host, self._port)
             self.client.stop()
             self.client.clear()
@@ -34,18 +34,18 @@ class Player(object):
         if self.volume == 100:
             return
         with mpc_mutex:
-            self.volume += 25
+            self._volume += 25
             self.client.connect(self._host, self._port)
-            self.client.setvol(self.volume)
+            self.client.setvol(self._volume)
             self.client.disconnect()
 
     def vol_down(self):
         if self.volume == 0:
             return
         with mpc_mutex:
-            self.volume -= 25
+            self._volume -= 25
             self.client.connect(self._host, self._port)
-            self.client.setvol(self.volume)
+            self.client.setvol(self._volume)
             self.client.disconnect()
 
     def pause(self):
@@ -61,3 +61,16 @@ class Player(object):
             self.client.play()
             self.client.disconnect()
             self.playing = True
+
+    def get_volume(self):
+        with mpc_mutex:
+            temp = self._volume
+        return temp
+
+    def get_name(self):
+        with mpc_mutex:
+            temp = self._name
+        return temp
+    def set_name(self, name):
+        with mpc_mutex:
+            self._name = name
