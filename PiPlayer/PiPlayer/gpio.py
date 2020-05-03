95f2f7ce1704523ab6a4938c4c0a8a9896555d5b
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-05-03 12:08:12 +0200

    Fix even more initial python mistakes

--- PiPlayer/PiPlayer/gpio.py
+++ PiPlayer/PiPlayer/gpio.py
@@ -7,14 +7,14 @@ _chip = gpiod.Chip('10008000.gpio')
 
 def gpio_thread(arg):
     print('Start thread')
-    button = _chip.getline(13)
+    button = _chip.get_line(13)
     button.request(consumer='PiPlayer', type=gpiod.LINE_REQ_EV_BOTH_EDGES)
     line = button.event_wait()
     if line:
         event = button.event_read()
         print(event)
 
-_thread = Thread(target = gpio_thread)
+_thread = Thread(target = gpio_thread, args = ('',))
 _thread.start()
 
 def thread_join():
